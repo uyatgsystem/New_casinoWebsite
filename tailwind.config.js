cbb23c3b994413acfcfb676fdaf1905e2ae80096
Muhammad-Ali252 <ma.atgsystems01@gmail.com>
2026-09-17 21:42:23 +0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,15 @@ module.exports = {
       },
       backgroundImage: {
         "custom-gradient":
-          "linear-gradient(90deg, #00F5D4 0%, #06B6D4 50%, #FF9F1C 100%)",
+          "linear-gradient(90deg, #2CD97D 0%, #06B6D4 50%, #FF9F1C 100%)",
         "custom-gradient-border":
-          "linear-gradient(0deg, #00F5D4 0%, #06B6D4 50%, #FF9F1C 100%)",
+          "linear-gradient(0deg, #2CD97D 0%, #06B6D4 50%, #FF9F1C 100%)",
       },
       colors: {
-        "gradient-start": "#00F5D4",
+        "gradient-start": "#2CD97D",
         "gradient-middle": "#06B6D4",
         "gradient-end": "#FF9F1C",
-        "cyber-primary": "#00F5D4",
+        "cyber-primary": "#2CD97D",
         "cyber-accent": "#06B6D4",
         "cyber-neon": "#FF9F1C",
         "cyber-dark": "#050811",
